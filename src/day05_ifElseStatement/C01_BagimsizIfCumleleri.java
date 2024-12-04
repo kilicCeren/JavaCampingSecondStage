11e7d5644bdaa3f403aab2584acd5afaae47096c
kilicCeren <kiliccrn07@gmail.com>
2024-12-04 21:07:46 +0300

    v1

---
+++ src/day05_ifElseStatement/C01_BagimsizIfCumleleri.java
@@ -0,0 +1,36 @@
+package day05_ifElseStatement;
+
+import java.util.Scanner;
+
+public class C01_BagimsizIfCumleleri {
+    public static void main(String[] args) {
+
+        // kullanicidan iki tamsayi isteyin
+        // eger 1. sayi daha buyukse, "Girilen ilk sayi daha buyuk",
+        // eger 2. sayi cift sayi ise "Girilen 2. sayi cift",
+        // eger 1. sayi pozitif sayi ise "Pozitif sayilar guzeldir" yazdirin
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lutfen iki tamsayi giriniz...");
+        int sayi1 = scanner.nextInt();
+        int sayi2 = scanner.nextInt();
+
+        // eger 1.sayi daha buyukse, "Girilen ilk sayi daha buyuk" yazdirin
+
+        if (sayi1 > sayi2) {
+            System.out.println("Girilen ilk sayi daha buyuk");
+        }
+
+        // eger 2.sayi cif sayi ise "Girilen 2.sayi cift" yazdirin
+
+        if (sayi2 % 2 == 0) {
+            System.out.println("Girilen 2.sayi cift");
+        }
+
+        // sayi1 pozitifse "Pozitif sayilar guzeldir"
+
+        if (sayi1 > 0) {
+            System.out.println("Pozitif sayilar guzeldir");
+        }
+    }
+}
