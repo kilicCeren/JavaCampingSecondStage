0e8cebdbd563c5b806fc4f5f8329b9ff898c2802
kilicCeren <kiliccrn07@gmail.com>
2024-12-13 21:28:25 +0300

    v6

---
+++ src/day06_NestedIfElseStatements_Ternary/C11_TernaryIle_EskenarUcgen.java
@@ -0,0 +1,22 @@
+package day06_NestedIfElseStatements_Ternary;
+
+import java.util.Scanner;
+
+public class C11_TernaryIle_EskenarUcgen {
+    public static void main(String[] args) {
+
+        // Kullanicidan bir ucgenin 3 kenar uzunlugunu alin,
+        // ucgen eskenar ise “Eskenar ucgen” yazdirin,
+        // degilse “Eskenar degil” yazdirin.
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Lutfen ucgenin kenar uzunluklarini giriniz...");
+
+        double kenar1 = scanner.nextDouble();
+        double kenar2 = scanner.nextDouble();
+        double kenar3 = scanner.nextDouble();
+
+        System.out.println(kenar1 == kenar2 & kenar2 == kenar3 ? "Eskenar ucgen" : "Eskenar degil");
+    }
+}
