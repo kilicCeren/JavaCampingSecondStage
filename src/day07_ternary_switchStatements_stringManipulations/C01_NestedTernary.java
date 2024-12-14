4566701087c656225039e9c49e8864dad2cd1b14
kilicCeren <kiliccrn07@gmail.com>
2024-12-14 15:21:10 +0300

    v7

---
+++ src/day07_ternary_switchStatements_stringManipulations/C01_NestedTernary.java
@@ -0,0 +1,27 @@
+package day07_ternary_switchStatements_stringManipulations;
+
+import java.util.Scanner;
+
+public class C01_NestedTernary {
+    public static void main(String[] args) {
+
+        // Kullanicidan bir sayi isteyin
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lutfen bir tam sayi giriniz: ");
+        int sayi = scan.nextInt();
+
+
+        // Sayi, 0'dan buyukse "pozitif"
+        // 0'dan buyuk degilse "pozitif degil" yazdirin
+
+        System.out.println(sayi > 0 ? "pozitif" : "pozitif degil");
+
+
+        // Sayi, 0'dan buyukse "pozitif",
+        // 0'dan kucukse "negatif",
+        // 0 ise "notr" yazdirin
+
+        System.out.println(sayi > 0 ? "pozitif" : (sayi < 0 ? "negatif" : "notr"));
+    }
+}
