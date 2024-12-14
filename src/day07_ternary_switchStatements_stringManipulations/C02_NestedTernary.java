90b4e4e99ba4439f3845acbbcba70b9332b08c28
kilicCeren <kiliccrn07@gmail.com>
2024-12-14 15:21:30 +0300

    v7

---
+++ src/day07_ternary_switchStatements_stringManipulations/C02_NestedTernary.java
@@ -0,0 +1,29 @@
+package day07_ternary_switchStatements_stringManipulations;
+
+import java.util.Scanner;
+
+public class C02_NestedTernary {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lutfen bir tam sayi giriniz:");
+        int sayi = scan.nextInt();
+
+
+        System.out.println(
+
+                sayi > 0
+
+                        ?
+
+                        sayi % 2 == 0 ? "cift sayi" : "cift sayi degil"  //sayi pozitif ise calisacak kodlar
+
+                        :
+
+                        sayi >= -999 && sayi <= -100 ? "3 basamakli sayi" : "3 basamakli sayi degil"   //sayi pozitif degilse calisacak kodlar
+
+        );
+
+        //  System.out.println( sayi > 0 ? sayi % 2 == 0 ? "cift sayi":"cift sayi degil":sayi>=-999 && sayi<=-100 ?"3 basamakli sayi":"3 basamakli sayi degil");
+    }
+}
