7349a17deb761ca05095f2c0b513548e51ea6789
kilicCeren <kiliccrn07@gmail.com>
2024-12-24 10:16:01 +0300

    v8

---
+++ src/day07_Ternary_SwitchStatements_StringManipulations/C09_ChartAt.java
@@ -0,0 +1,29 @@
+package day07_Ternary_SwitchStatements_StringManipulations;
+
+public class C09_ChartAt {
+    public static void main(String[] args) {
+
+        String str = "Java Candir";
+
+        System.out.println(str.charAt(2)); // v
+
+        /*
+            Java'da karakter sayisi ve index farklidir
+
+            kac karakter oldugunu kontrol ederken 1'den basliyoruz
+            ama index icin 0'dan basliyoruz
+
+            bir string'de son index (karakter sayisi - 1) dir
+         */
+
+        // bastan 6. karakteri yazdirin
+        System.out.println(str.charAt(5)); // C
+
+        // son karakteri yazdirin
+        System.out.println(str.charAt(10)); // r
+
+        // Olmayan bir index kullanilirsa
+        System.out.println(str.charAt(13)); // StringIndexOutOfBoundsException
+
+    }
+}
---
+++ src/day07_Ternary_SwitchStatements_StringManipulations/C10_Length.java
@@ -0,0 +1,32 @@
+package day07_Ternary_SwitchStatements_StringManipulations;
+
+public class C10_Length {
+    public static void main(String[] args) {
+
+
+        String str = "Java Candir";
+
+        // eger kullanilan karakter sayisini gormek istersek
+        System.out.println(str.length()); // 11
+
+
+        // son karakteri yazdirin
+        System.out.println(str.charAt(str.length() - 1)); // r
+
+
+        // sondan 3. karakteri yazdirin
+        System.out.println(str.charAt(str.length() - 3)); // d String str = "Java Candir";
+
+
+        // eger kullanilan karakter sayisini gormek istersek
+        System.out.println(str.length()); // 11
+
+
+        // son karakteri yazdirin
+        System.out.println(str.charAt(str.length() - 1)); // r
+
+
+        // sondan 3. karakteri yazdirin
+        System.out.println(str.charAt(str.length() - 3)); // d
+    }
+}
