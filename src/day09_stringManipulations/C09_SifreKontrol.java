7c17fe376ca4115b9619dfb739dfa7c6958ca3f8
kilicCeren <kiliccrn07@gmail.com>
2024-12-31 15:46:56 +0300

    v2

---
+++ src/day09_stringManipulations/C09_SifreKontrol.java
@@ -0,0 +1,51 @@
+package day09_stringManipulations;
+
+import java.util.Scanner;
+
+public class C09_SifreKontrol {
+    public static void main(String[] args) {
+
+        // Soru : Kullanicidan bir sifre isteyip,
+        // asagidaki sartlari kontrol edin
+        // ve kullaniciya duzeltmesi gereken tum eksikleri soyleyin,
+        // eger tum sartlari saglarsa, "sifre basariyla kaydedildi" yazdirin
+        //  - ilk harf kucuk harf olmali
+        //  - son karakter rakam olmali
+        //  - sifre bosluk icermemeli
+        //  - uzunlugu en az 10 karakter olmali
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lutfen sifrenizi giriniz: ");
+        String sifre = scan.nextLine();
+
+        int sayac = 0;
+
+        if (!Character.isLowerCase(sifre.charAt(0))) {
+            System.out.println("Ilk harf kucuk harf olmali");
+            sayac++;
+        }
+
+        if (!Character.isDigit(sifre.charAt(sifre.length() - 1))) {
+            System.out.println("Son karakter rakam olmali");
+            sayac++;
+        }
+
+        if (sifre.contains(" ")) {
+            System.out.println("Sifre bosluk icermemeli");
+            sayac++;
+        }
+
+        if (!(sifre.length() >= 10)) {
+            System.out.println("Sifre uzunlugu en az 10 karakter olmali");
+            sayac++;
+        }
+
+        // sayac 0-4 arasi olabilir
+        // eger 0'sa hata yok demektir
+        // eger 4'se hepsi hatali
+
+        if (sayac == 0) {
+            System.out.println("Sifre basariyla kaydedildi");
+        }
+    }
+}
